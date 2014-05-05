02742cfc7c58f72e3b397717d2f8b4934ccec50b
Jason Carr <jason.s.carr@gmail.com>
2014-05-04 19:02:31 -0700

    Don't use os env var in unit tests.

--- tests/unit/test_jobs.py
+++ tests/unit/test_jobs.py
@@ -8,8 +8,8 @@ import mock
 
 from sauce import sauce
 
-sauce_user = os.environ['SAUCE_USERNAME']
-sauce_key = os.environ['SAUCE_ACCESS_KEY']
+sauce_user = 'test-user'
+sauce_key = '_'
 
 
 @mock.patch.object(sauce.Sauce, 'request')
@@ -20,7 +20,7 @@ class TestJobs(unittest.TestCase):
         cls.sauce = sauce.Sauce(sauce_user, sauce_key)
 
     def test_list_jobs(self, mock_request):
-        jobs_url = '/rest/v1/saucelabs-python/jobs'
+        jobs_url = '/rest/v1/test-user/jobs'
         self.sauce.jobs.list_jobs()
         mock_request.assert_called_with('GET', jobs_url,
                                         params={})
@@ -35,7 +35,7 @@ class TestJobs(unittest.TestCase):
     def test_get_job_details(self, mock_request):
         self.sauce.jobs.get_job_details('123')
         mock_request.assert_called_with(
-            'GET', '/rest/v1/saucelabs-python/jobs/123')
+            'GET', '/rest/v1/test-user/jobs/123')
 
     def test_update_job(self, mock_request):
         self.sauce.jobs.update_job('123', build='456',
@@ -43,7 +43,7 @@ class TestJobs(unittest.TestCase):
                                    name='test name', passed='False',
                                    public='False', tags=['tag1', 'tag2'])
         mock_request.assert_called_with('PUT',
-                                        '/rest/v1/saucelabs-python/jobs/123',
+                                        '/rest/v1/test-user/jobs/123',
                                         '{"name": "test name", '
                                         '"tags": ["tag1", "tag2"], '
                                         '"custom-data": {"my-data": "789"}, '
@@ -53,24 +53,24 @@ class TestJobs(unittest.TestCase):
     def test_stop_job(self, mock_request):
         self.sauce.jobs.stop_job('123')
         mock_request.assert_called_with(
-            'PUT', '/rest/v1/saucelabs-python/jobs/123/stop')
+            'PUT', '/rest/v1/test-user/jobs/123/stop')
 
     def test_delete_job(self, mock_request):
         self.sauce.jobs.delete_job('123')
         mock_request.assert_called_with(
-            'DELETE', '/rest/v1/saucelabs-python/jobs/123')
+            'DELETE', '/rest/v1/test-user/jobs/123')
 
     def test_list_job_assets(self, mock_request):
         self.sauce.jobs.list_job_assets('123')
         mock_request.assert_called_with(
-            'GET', '/rest/v1/saucelabs-python/jobs/123/assets')
+            'GET', '/rest/v1/test-user/jobs/123/assets')
 
     @mock.patch.object(sauce.Sauce, 'download')
     def test_download_job_asset(self, mock_download, mock_request):
         self.sauce.jobs.download_job_asset('123', 'file.log')
         mock_request.assert_not_called()
-        mock_download.assert_called_with('/rest/v1/saucelabs-python/jobs/123/assets/file.log')
+        mock_download.assert_called_with('/rest/v1/test-user/jobs/123/assets/file.log')
 
     def test_delete_job_assets(self, mock_request):
         self.sauce.jobs.delete_job_assets('123')
-        mock_request.assert_called_with('DELETE', '/rest/v1/saucelabs-python/jobs/123/assets')
+        mock_request.assert_called_with('DELETE', '/rest/v1/test-user/jobs/123/assets')
