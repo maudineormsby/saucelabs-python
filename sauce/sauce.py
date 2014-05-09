dbbd0b931162fed28eeddaa30988ea72088ba972
Jason Carr <jason.s.carr@gmail.com>
2014-05-08 19:44:44 -0700

    Add temporary storage upload

--- sauce/sauce.py
+++ sauce/sauce.py
@@ -1,3 +1,5 @@
+import os
+
 import requests
 
 import jobs
@@ -23,3 +25,23 @@ class Sauce(object):
         resp = requests.get(url)
         resp.raise_for_status()
         return resp.content
+
+    def upload_file(self, user_file, dest_file=None, overwrite=False):
+        """
+        Uploads a file to sauce's temporary storage.
+
+        Args:
+          user_file: relative or absolute path to the file to upload to sauce.
+          dest_file: filename to use in sauce's system.
+          overwrite: whether the file in sauce's system should be overwritten.
+
+        Returns:
+          None
+        """
+        if not dest_file:
+            file_name = os.path.basename(user_file)
+        url = self.base_url + '/storage/{0}/{1}'.format(self.user, file_name)
+        user_file_path = os.path.abspath(user_file)
+        with open(user_file_path) as upload_data:
+            headers = {'Content-Type': 'application/octet-stream'}
+            requests.post(url, data=upload_data, headers=headers)
