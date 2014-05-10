3c0c78b9ca8955baf3c2676dc89f277b5c44f47f
Jason Carr <jason.s.carr@gmail.com>
2014-05-09 21:55:21 -0700

    Fix up some helpers and add travis_only decorator.

--- tests/functional/helpers/sauce_helpers.py
+++ tests/functional/helpers/sauce_helpers.py
@@ -2,13 +2,17 @@
 Helper functions used by functional tests.
 """
 
+import functools
 import os
 import time
 
+from nose.plugins.attrib import attr
+from nose.plugins.skip import SkipTest
 from selenium import webdriver
 
-sauce_user = os.environ['SAUCE_USERNAME']
-sauce_key = os.environ['SAUCE_ACCESS_KEY']
+sauce_user = os.getenv('SAUCE_USERNAME')
+sauce_key = os.getenv('SAUCE_ACCESS_KEY')
+travis = os.getenv('TRAVIS', False)
 
 
 def create_session():
@@ -19,3 +23,12 @@ def create_session():
         sauce_url, webdriver.DesiredCapabilities.FIREFOX)
     driver.ctime = ctime
     return driver
+
+
+def travis_only(func):
+    @functools.wraps(func)
+    def run_test(*args, **kwargs):
+        if not travis:
+            raise SkipTest('Tunnel tests are run in travis-ci only.')
+        func(*args, **kwargs)
+    return attr('travis_only')(run_test)
